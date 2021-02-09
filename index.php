d40c29d2269aa4f42a07b0dba89c735c7b9e2153
Arpit Sironiya <64483012+farista2@users.noreply.github.com>
2021-02-09 12:38:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <!--  <img src="https://raw.githubusercontent.com/farista2/test/master/Docker.png" alt="Girl in a jacket"> -->
-<img src="https://raw.githubusercontent.com/farista2/images/main/PICNIC_20190811_233826185.jpg">
+
+<div style="background-image: url('https://raw.githubusercontent.com/farista2/images/main/PICNIC_20190811_233826185.jpg');">
 HEllo Ansible, this is testing code
 <pre>
 <?php
