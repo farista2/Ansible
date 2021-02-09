1d9c767d45590c92c8f2394f5fa15f9bfd8aea97
Arpit Sironiya <64483012+farista2@users.noreply.github.com>
2021-02-09 12:00:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 
-<img src="https://raw.githubusercontent.com/farista2/test/master/Docker.png" alt="Girl in a jacket">
+//<img src="https://raw.githubusercontent.com/farista2/test/master/Docker.png" alt="Girl in a jacket">
+<img src="https://raw.githubusercontent.com/farista2/images/main/PICNIC_20190811_233826185.jpg">
 HEllo Ansible, this is testing code
 <pre>
 <?php
